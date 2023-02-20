97524f617b3e99d9124e23824c6f136d28978688
tiiingaling <93777306+tiiingaling@users.noreply.github.com>
2023-02-20 21:33:45 +0000

    added base example from activity 24

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p> My name is Ting Wong, I am seeking opportunities 
+      </p>
+    </div>
+  );
+}
---
+++ src/components/pages/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>Blog Page</h1>
+      <p>
+      </p>
+    </div>
+  );
+}
