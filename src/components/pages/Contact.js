0178a142feb7f455a56d448956dfeb1860eaedcb
tiiingaling <93777306+tiiingaling@users.noreply.github.com>
2023-03-05 19:02:19 +0000

    added contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Contact() {
+function ContactForm() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState);
+    setFormState({ name: '', email: '', message: '' });
+  };
+
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <a href="https://github.com/tiiingaling">tiiingaling's GitHub profile</a>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          name="name"
+          value={formState.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          name="email"
+          value={formState.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="message">Message:</label>
+        <textarea
+          className="form-control"
+          id="message"
+          name="message"
+          value={formState.message}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">Send Message</button>
+    </form>
   );
 }
+
+export default ContactForm;
