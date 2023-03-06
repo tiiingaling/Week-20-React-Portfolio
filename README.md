c9caaa08e20973558f1479f1dd9f80394d66e7a0
tiiingaling <93777306+tiiingaling@users.noreply.github.com>
2023-03-06 22:57:08 +0000

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Week-20-React-Portfolio
+
+## Description
+This week's challenge is to portfolio using your new React skills, which will help set you apart from other developers whose portfolios don’t use the latest technologies. 
+
+
+## Screenshots
+[!Screenshot of the About Page](./public/assets/screenshot-about.png)
+[!Screenshot of the Portfolio Page](./public/assets/screenshot-portfolio.png)
+
+## Deployed Link
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,8 +5,11 @@ export default function About() {
   return (
     <div>
       <h1>About Page</h1>
+      <p> My name is Ting Wong, I am a bootcamp student seeking opportunities in the tech world!
+        In order to convince you to hire me, this is a cute logo of me!
+      </p>
       <img src="/assets/ting-icon.png" alt="Icon for Ting" class="img-fluid w-50"></img>
-      <p> My name is Ting Wong, I am a bootcamp student seeking opportunities in the tech world!</p>
+      
     </div>
   );
 }
