f2e309c58aa8df2563bf617f686ed45b6526a969
tiiingaling <93777306+tiiingaling@users.noreply.github.com>
2023-03-06 22:03:27 +0000

    cleanup, added header title, styled navbar

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,48 +2,47 @@ import React from 'react';
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs justify-content-center ">
-      <li className="nav-item-danger">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-
-          className={currentPage === 'About' ? 'nav-link active tab' : 'nav-link tab'}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#portfolio"
-          onClick={() => handlePageChange('Portfolio')}
-
-          className={currentPage === 'Portfolio' ? 'nav-link active tab' : 'nav-link tab'}
-        >
-          Portfolio
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active tab' : 'nav-link tab'}
-        >
-          Contact
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#resume"
-          onClick={() => handlePageChange('Resume')}
-
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </a>
-      </li>
-    </ul>
+    <header>
+      <h1 className='text-center'>Ting Wong</h1>
+      <ul className="nav nav-tabs justify-content-center ">
+        <li className="nav-item-danger">
+          <a
+            href="#about"
+            onClick={() => handlePageChange('About')}
+            className={currentPage === 'About' ? 'nav-link active tab' : 'nav-link tab'}
+          >
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#portfolio"
+            onClick={() => handlePageChange('Portfolio')}
+            className={currentPage === 'Portfolio' ? 'nav-link active tab' : 'nav-link tab'}
+          >
+            Portfolio
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange('Contact')}
+            className={currentPage === 'Contact' ? 'nav-link active tab' : 'nav-link tab'}
+          >
+            Contact
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#resume"
+            onClick={() => handlePageChange('Resume')}
+            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
+    </header>
   );
 }
 
--- src/components/projectCards.js
+++ src/components/projectCards.js
@@ -6,7 +6,7 @@ const Portfolio = () => {
     {
       id: 1,
       title: 'J.A.T.E',
-      image: 'https://via.placeholder.com/400x250',
+      image: '/assets/jate.png',
       description: 'Just Another Text Editor',
       tags: ['React', 'Javascript', 'PWA'],
       url: 'https://infinite-escarpment-70643.herokuapp.com',
@@ -14,42 +14,42 @@ const Portfolio = () => {
     {
       id: 2,
       title: 'Note Taker',
-      image: 'https://via.placeholder.com/400x250',
-      description: '',
+      image: '/assets/note-taker.png',
+      description: 'A note taking React app with save and delete functions',
       tags: ['React', 'Express', 'API'],
       url: 'https://vast-shelf-36759.herokuapp.com/',
     },
     {
       id: 3,
       title: 'Techblog',
-      image: 'https://via.placeholder.com/400x250',
-      description: '',
+      image: '/assets/techblog.png',
+      description: 'A CVS style blog about tech!',
       tags: ['MySQL', 'Node', 'Express'],
       url: 'https://aqueous-reaches-57069.herokuapp.com/',
     },
     {
       id: 4,
-      title: 'Project 4',
-      image: 'https://via.placeholder.com/400x250',
-      description: '',
+      title: 'Rock Paper Scissors',
+      image: '/assets/rps.png',
+      description: 'Simple Rock Paper Scissors game',
       tags: ['React', 'Material UI', 'HTML'],
-      url: '#',
+      url: '',
     },
     {
       id: 5,
-      title: 'Project 5',
-      image: 'https://via.placeholder.com/400x250',
-      description: '',
-      tags: ['React', 'Material UI', 'HTML'],
-      url: '#',
+      title: 'Discord Bot',
+      image: '/assets/discord2.png',
+      description: 'Bot for discord to play music via Spotify',
+      tags: ['React', 'Javascript', 'Java'],
+      url: 'https://discord.com/',
     },
     {
       id: 6,
-      title: 'Project 6',
-      image: 'https://via.placeholder.com/400x250',
-      description: '',
+      title: 'IFTT',
+      image: '/assets/iftt.png',
+      description: 'IFTT script to automate Spotify playlist',
       tags: ['React', 'Material UI', 'HTML'],
-      url: '#',
+      url: 'https://ifttt.com/',
     },
   ];
 
@@ -57,9 +57,9 @@ const Portfolio = () => {
     <main className="container">
       {projects.map((project) => (
         <div className="card" key={project.id}>
-          <img src={project.image} alt={project.title} />
+          <img src={project.image} alt={project.title} width="400" height="250"/>
           <h2>{project.title}</h2>
-          <p>{project.description}</p>
+          <p className="card-desc">{project.description}</p>
           <ul className="tags">
             {project.tags.map((tag) => (
               <li key={tag}>{tag}</li>
